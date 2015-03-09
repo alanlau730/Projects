08405b2c017d00d0d3a84781ede35edaabef29bb
Alan Lau <alanlau@usc.edu>
2015-03-09 04:32:59 -0700

    Added some source code. Still not yet dynamic

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,4 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//simple fibonacci number program with dynamic programming
 
 public class Fibonacci {
-
+	private long []array;
+	private int n;
+	private int index;
+	
+	
+	public Fibonacci(int n){
+		array = new long[n];
+		Arrays.fill(array, -1); //initialize to -1...meaning element does not yet exist
+		this.n = n;
+		this.index = n;
+	}
+	
+	public long compute(){
+		return Helper(n);
+	}
+	
+	private long Helper(int n){//will clean up code
+	  
+		if (n < 1)
+			return 0;		
+		if (n == 1) // base case
+			return 1;
+		else
+			return Helper(n-1) + Helper(n-2);
+	}
+	
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the nth number of the Fibonacci sequence: ");
+		int n = scan.nextInt();
+		Fibonacci fb = new Fibonacci(n);
+		System.out.println("Result is: " + fb.compute());		
+		scan.close();
+		
+	}
 }
