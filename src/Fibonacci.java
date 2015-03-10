c2f8d0a44b5c30b7651c9218412da4e6375f5514
Alan Lau <alanlau@usc.edu>
2015-03-10 01:27:24 -0700

    Fibonacci should be ready. Might add some code again later.

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,45 +4,66 @@ import java.util.Scanner;
 //simple fibonacci number program with dynamic programming
 //will add timing shortly comparing dynamic vs non-dynamic
 
+//as the value of n increases the dynamic program becomes noticeably faster
+
 public class Fibonacci {
 	private long []array;
 	private int n;
-	private int index;
+	
 	
 	
 	public Fibonacci(int n){
 		array = new long[n];
 		Arrays.fill(array, -1); //initialize to -1...meaning element does not yet exist
 		this.n = n;
-		this.index = n;
+		
 	}
 	
-	public long compute(){
-		return Helper(n);
+	public long compute(boolean dynamic){
+		long start = System.currentTimeMillis();
+		//System.out.println("Start time: " + System.currentTimeMillis());
+		long result = Helper(n,dynamic);
+		if (dynamic)
+			System.out.println("Total dynamic time: " + (System.currentTimeMillis()-start) + "ms");
+		else
+			System.out.println("Total normal time: " + (System.currentTimeMillis()-start)+"ms");
+		return result;
 	}
 	
-	private long Helper(int n){//will clean up code
-	  if (n == 1 || n == 2)
-		  //array[n-1] = 1; //caching
+	private long Helper(int n,boolean dynamic){//will clean up code
+	  if (n == 1 || n == 2)	
 		  return 1;
 	  
 	  else{
-		  long helpOne;
-		  long helpTwo;
-		  
-		  if(array[n-2] != -1)//see if value is already cached or not
-			  helpOne = array[n-2];
+			  if(dynamic){
+				  long helpOne = 0;
+				  long helpTwo = 0;
+				  
+				 if(array[n-2] == -1){//see if value is already cached or not
+					 helpOne = Helper(n-1,dynamic);
+					 array[n-2] = helpOne;
+			  	 }
+				 else
+					 helpOne = array[n-2];
+				 
+				 System.out.println(helpOne + " HELP ONE ");
+				  
+				  if(array[n-3]==-1){
+					  helpTwo = Helper(n-2,dynamic);					  
+					  array[n-3] = helpTwo;
+				  }
+				  
+				  else
+					  helpTwo = array[n-3];
+				  
+				  System.out.println(helpTwo + " HELP two ");
+				 				  
+				  return helpOne+helpTwo;
+			  }
 		  else
-			  helpOne = Helper(n-1);
-		  
-		  if(array[n-3]!=-1)
-			  helpTwo = array[n-3];
-		  else
-			  helpTwo = Helper(n-2);
-		  
-		  return helpOne+helpTwo;
+			  return Helper(n-1,dynamic) + Helper(n-2,dynamic);//without caching
 	  	}
-			
+	  		
 	}
 	
 	public static void main(String[]args){
@@ -50,7 +71,7 @@ public class Fibonacci {
 		System.out.println("Enter the nth number of the Fibonacci sequence: ");
 		int n = scan.nextInt();
 		Fibonacci fb = new Fibonacci(n);
-		System.out.println("Result is: " + fb.compute());		
+		System.out.println(n + "th number.Dynamic is: " + fb.compute(true) + " Normal is: " + fb.compute(false));		
 		scan.close();
 		
 	}
